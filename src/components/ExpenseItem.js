a47f8ee2c8f793805d56870305ce9c34cb891d5f
simoneguimaraes <simoneguimaraes11@gmail.com>
2022-01-11 19:14:31 -0300

    updated expense item comp

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,12 @@
 import "./ExpenseItem.css"
 
-function ExpenseItem() {
-    const expenseDate = new Date (2022, 0, 10)
-
-
+function ExpenseItem(props) {
     return ( 
         <div className="expense-item">
-            <div >January 10th 2022</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$294.99</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
 
