998df546249fc8d28a6ff13e9a401ecfcd348960
simoneguimaraes <simoneguimaraes11@gmail.com>
2022-01-15 09:57:43 -0300

    created new expense form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Expenses from "./components/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
     {
       id: "e1",
-      title: "Gym Membership",
+      title: "Gym Membership",  
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
@@ -30,6 +31,7 @@ function App() {
 
   return (
     <div className="App">
+      <NewExpense/>
       <Expenses
         expenses={expenses}
       />
